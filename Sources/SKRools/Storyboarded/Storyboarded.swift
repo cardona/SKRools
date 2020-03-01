d1f1e49491fbf570c6e3b684962b72e57bfdeb6d
Oscar Cardona <ocardona@omitsis.com>
2020-03-01 12:56:43 +0100

    remove some comments

--- Sources/SKRools/Storyboarded/Storyboarded.swift
+++ Sources/SKRools/Storyboarded/Storyboarded.swift
@@ -15,13 +15,8 @@ public protocol Storyboarded {
 
 extension Storyboarded where Self: UIViewController {
     static public func instantiate(storyboardName: String = "Main") -> Self {
-        // this pulls out "MyApp.MainViewController"
         let fullName = NSStringFromClass(self)
-
-        // this splits by the dot and use everything after, giving "MainViewController"
         let className = fullName.components(separatedBy: ".")[1]
-
-        // load our storyboard
         let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main)
 
         // instantiate a view controller with that identifier, and force cast as the type was requested
