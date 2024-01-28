880e7fad367df7a41d3dae48ec7f87cbe172d2a1
Oscar Cardona <ocardona@gmail.com>
2024-01-28 20:38:22 +0100

    Add SKConfig documentation and unit testing

--- Sources/SKRools/SKRoolsConfig.swift
+++ Sources/SKRools/SKRoolsConfig.swift
@@ -1,6 +1,6 @@
 //
 //  SKRoolsConfig.swift
-//  
+//
 //
 //  Created by Oscar Cardona on 21/3/22.
 //  Copyright © 2020 Cardona.tv. All rights reserved.
@@ -9,65 +9,69 @@
 
 import Foundation
 
+/// `SKRoolsConfig` is a singleton class designed to manage and provide centralized access
+/// to application-wide configurations. It offers separate modules for network and logger configurations.
+/// Use the shared instance to configure and retrieve settings throughout the application.
+///
+/// Usage:
+/// ```
+/// AppConfiguration.shared.networkBaseURL = "https://api.example.com"
+/// AppConfiguration.shared.loggerDebugGroups = [.networking, .secureEnclave, .system ....]
+/// ```
+///
+/// - Note: The class is thread-safe and can be accessed from multiple threads.
 public final class SKRoolsConfig {
     public static let shared = SKRoolsConfig()
-    private lazy var currentBaseURL = "noURLBase!"
-    private var currentApikeyPublic: String?
-    private var currentApikeyPrivate: String?
-    private var currentCert: String?
-    private lazy var currentLoggerGroup: [DebugGroup] = [.networking,
-                                                         .filesystem,
-                                                         .system,
-                                                         .secureEnclave,
-                                                         .keychaing,
-                                                         .configuration,
-                                                         .token,
-                                                         .parse]
     
-}
-
-// MARK: - Network Config
-extension SKRoolsConfig {
-    public func config(url: String) {
-        currentBaseURL = url
+    // Network Configuration
+    private struct NetworkConfig {
+        var baseURL = ""
+        var publicApiKey = ""
+        var privateApiKey = ""
+        var certificate = ""
     }
     
-    public func config(apikeyPublic: String) {
-        currentApikeyPublic = apikeyPublic
+    private var networkConfig = NetworkConfig()
+    
+    // Logger Configuration
+    private struct LoggerConfig {
+        var debugGroups: [DebugGroup] = [.networking]
     }
     
-    public func config(apikeyPrivate: String) {
-        currentApikeyPrivate = apikeyPrivate
-    }
-
-    public func config(cert: String) {
-        currentCert = cert
-    }
-
-    func baseURL() -> String {
-        return currentBaseURL
+    private var loggerConfig = LoggerConfig()
+    
+    // Private initializer to prevent external instantiation.
+    private init() {}
+    
+    /// Network base URL. Used to configure the base URL for network requests.
+    public var networkBaseURL: String {
+        get { networkConfig.baseURL }
+        set { networkConfig.baseURL = newValue }
     }
     
-    public func apikeyPublic() -> String? {
-        return currentApikeyPublic
+    /// Public API key. Used for identifying the client in network requests.
+    public var networkPublicApiKey: String {
+        get { networkConfig.publicApiKey }
+        set { networkConfig.publicApiKey = newValue }
     }
     
-    public func apikeyPrivate() -> String? {
-        return currentApikeyPrivate
-    }
-
-    public func cert() -> String? {
-        return currentCert
-    }
-}
-
-// MARK: - Logger Config
-extension SKRoolsConfig {
-    public func config(loggerGroups: [DebugGroup]) {
-        currentLoggerGroup = loggerGroups
+    /// Private API key. Used for secure operations and authentication.
+    /// - Warning: Keep this key secure and avoid exposing it in logs or UI.
+    public var networkPrivateApiKey: String {
+        get { networkConfig.privateApiKey }
+        set { networkConfig.privateApiKey = newValue }
     }
     
-    func debugGroups() -> [DebugGroup] {
-        return currentLoggerGroup
+    /// SSL certificate string. Used for network security.
+    public var networkCertificate: String {
+        get { networkConfig.certificate }
+        set { networkConfig.certificate = newValue }
+    }
+    
+    /// Array of `DebugGroup` for logger configuration.
+    /// Use this to specify which debugging groups should be active.
+    public var loggerDebugGroups: [DebugGroup] {
+        get { loggerConfig.debugGroups }
+        set { loggerConfig.debugGroups = newValue }
     }
 }
